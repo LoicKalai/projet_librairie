76f65503173f3c21b01f814d802bbff035e37667
LoicKalai <loic.kalaidjoglou@icloud.com>
2022-04-19 14:05:15 +0200

    [Insert]
    insertion de la fonction length

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>Ma Librairie</h1>
 </body>
 </html>
---
+++ str_lib.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'index.php';
+
+
+
+function strg_len($str){
+$count = 1;
+
+    for ($i = 0; $i < $str.length; $i++) {
+        if ($str == "") {
+            $count++;
+        }
+    }
+}
+echo (strg_len("salut"));
+
+?>
